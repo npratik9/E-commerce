1f570dd8d987fefeafaa638aa9a9c3ef35e0f774
pratik niraula <npratik021@gmail.com>
2025-12-26 10:27:37 +0545

    mongoDb schema and model created

--- backend/src/config/constants.js
+++ backend/src/config/constants.js
@@ -3,6 +3,21 @@ const Status = {
     INACTIVE:"inactive"
 }
 
+const UserRoles = {
+    ADMIN: "admin",
+    OWNER: "owner",
+    CUSTOMER: "customer"
+}
+
+const Gender ={
+    MALE: "male",
+    Female: "female",
+    OTHER: "other"
+}
+
+
 module.exports = {
-    Status
+    Status,
+    UserRoles,
+    Gender
 }
---
+++ backend/src/user/user.model.js
@@ -0,0 +1,80 @@
+const mongoose = require("mongoose");
+const { UserRoles, Status } = require("../config/constants");
+
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 50,
+      trim: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+    },
+
+    role: {
+      type: String,
+      enum: Object.values(UserRoles),
+      default: UserRoles.CUSTOMER,
+    },
+
+    image: {
+      id: String,
+      optimizedUrl: String,
+      originalUrl: String,
+    },
+
+    address: String,
+
+    phone: String,
+
+    gender: {
+      type: String,
+      enum: Object.values(Gender),
+    },
+
+    activationToken: String,
+
+    expiry: Date,
+
+    forgetToken: String,
+
+    status: {
+      type: String,
+      enum: Object.values(Status),
+      default: Status.INACTIVE,
+    },
+
+    createdBy: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      default: null,
+    },
+
+    updatedBy: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      default: null,
+    },
+  },
+  {
+    timestamps: true,
+    autoCreate: true,
+    autoIndex: true,
+  }
+);
+
+//model
+const UserModel = mongoose.model("User", UserSchema);
+module.exports = UserModel;
